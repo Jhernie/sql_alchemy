bc329110381b2f4dcb660aa31e0fe491fad226b3
Jhernie Evangelista <evangelista.jhernie@gmail.com>
2022-11-08 21:31:46 -1000

    adding .py file

---
+++ app.py
@@ -0,0 +1,42 @@
+#import dependencies
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#build engine and map keys
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#build app
+app = Flask(__name__)
+
+#build routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation_gauge():
+    print("/api/v1.0/precipitation - route hit")
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs" 
+    )
+
+
+app.run(debug=True)
+
